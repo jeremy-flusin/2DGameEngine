b65919e1e4f8359ef0a1d66dff3ff30f3d907ded
jeremy-flusin <flusin.jeremy@gmail.com>
2013-12-20 01:48:04 +0100

    light improvment

--- src/com/jflusin/engine/entities/lights/Light.java
+++ src/com/jflusin/engine/entities/lights/Light.java
@@ -1,4 +1,4 @@
-package  com.jflusin.engine.entities.lights;
+package com.jflusin.engine.entities.lights;
 
 import java.awt.Color;
 import java.awt.event.KeyEvent;
@@ -12,11 +12,11 @@ import com.jflusin.engine.scenes.AbstractScene;
 public class Light extends AbstractEntity {
 
 	int _radius;
-	int _precision = 10000;
+	int _precision = 100;
 	float _opacity = 0.1f;
 	Color _color;
 	AbstractEntity _linkedEntity = null;
-	
+
 	public Light(int pX, int pY, int pRadius, float pOpacity, Color pColor,
 			AbstractScene pScene) {
 		super(pX, pY, pRadius, pRadius, pScene);
@@ -28,87 +28,106 @@ public class Light extends AbstractEntity {
 	@Override
 	public void draw(GLAutoDrawable pDrawable) {
 		GL2 gl = pDrawable.getGL().getGL2();
-		
+
 		gl.glBlendFunc(GL2.GL_SRC_ALPHA, GL2.GL_ONE);
 		gl.glDisable(GL2.GL_DEPTH_TEST);
 		gl.glEnable(GL2.GL_BLEND);
-		
+
 		gl.glEnable(GL2.GL_POINT_SMOOTH);
 		gl.glHint(GL2.GL_POINT_SMOOTH_HINT, GL2.GL_NICEST);
-		
-		drawCircle(gl);
+		gl.glShadeModel(GL2.GL_SMOOTH);
+        float incr = (float) (2 * Math.PI / _precision);
+
+        gl.glBegin(GL2.GL_TRIANGLE_FAN);
+
+			 gl.glColor4f((float)_color.getRed()/255,
+			 (float)_color.getGreen()/255,
+			 (float)_color.getBlue()/255,
+			 _opacity);
+              gl.glVertex2f(getX(), getY());
+
+ 			 gl.glColor4f((float)_color.getRed()/255,
+ 					 (float)_color.getGreen()/255,
+ 					 (float)_color.getBlue()/255,
+ 					 0);
+
+              for(int i = 0; i < _precision; i++)
+              {
+                    float angle = incr * i;
+
+                    float x = getX()+ (float) Math.cos(angle) * _radius;
+                    float y = getY()+(float) Math.sin(angle) * _radius;
+
+                    gl.glVertex2f(x, y);
+              }
+
+              gl.glVertex2f(_radius, getY());
+
+        gl.glEnd();
 		gl.glDisable(GL2.GL_BLEND);
 	}
 
 	@Override
 	public void update() {
-		if(_scene.getSceneFactory().getGame().getKeyboardListener().isKeyPressed(KeyEvent.VK_ADD)){
-			_opacity += 0.0001f;
+		if (_scene.getSceneFactory().getGame().getKeyboardListener()
+				.isKeyPressed(KeyEvent.VK_ADD)) {
+			_opacity+= 0.05f;
 		}
-		if(_scene.getSceneFactory().getGame().getKeyboardListener().isKeyPressed(KeyEvent.VK_ENTER)){
-			_opacity -= 0.0001f;
+		if (_scene.getSceneFactory().getGame().getKeyboardListener()
+				.isKeyPressed(KeyEvent.VK_ENTER)) {
+			_opacity-= 0.05f;
 		}
-		
+
 	}
 
-	void drawCircle(GL2 gl)
-	{
-	 
-	   for (int i=0; i < _precision; i++)
-	   {
-	      float degInRad = (float)i * (float)(2 * Math.PI/_precision);
-	      gl.glShadeModel(GL2.GL_SMOOTH);
-	      gl.glBegin(GL2.GL_LINES);
-	      
-			gl.glColor4f((float)_color.getRed()/255,
-					(float)_color.getGreen()/255,
-					(float)_color.getBlue()/255,
-					_opacity);
-	      	gl.glVertex2d(getX(), getY());				
-	      	gl.glVertex2d(getX()+ Math.cos(degInRad) * _radius/2, getY() + Math.sin(degInRad) * _radius/2);
-			gl.glColor4f((float)_color.getRed()/255,
-					(float)_color.getGreen()/255,
-					(float)_color.getBlue()/255,
-					_opacity);
-	      	gl.glVertex2d(getX()+ Math.cos(degInRad) * _radius/2, getY() + Math.sin(degInRad) * _radius/2);
-			gl.glColor4f((float)_color.getRed()/255,
-					(float)_color.getGreen()/255,
-					(float)_color.getBlue()/255,
-					0);
-		 	gl.glVertex2d(getX()+ Math.cos(degInRad) * _radius, getY() + Math.sin(degInRad) * _radius);
-	   }
-	 
-	   gl.glEnd();
+	void drawCircle(GL2 gl) {
+
+		gl.glShadeModel(GL2.GL_SMOOTH);
+		gl.glBegin(GL2.GL_POLYGON);
+		for (int i = 0; i < 628; i++) {
+			float degInRad = (float) i / 100;
+			float realOpacity = _opacity * 100;
+			gl.glColor4f((float) _color.getRed() / 255,
+					(float) _color.getGreen() / 255,
+					(float) _color.getBlue() / 255, realOpacity);
+			 gl.glVertex2d(getX(), getY());
+//			 gl.glColor4f((float)_color.getRed()/255,
+//			 (float)_color.getGreen()/255,
+//			 (float)_color.getBlue()/255,
+//			 0);
+			gl.glVertex2d(getX() + Math.cos(degInRad) * _radius,
+					getY() + Math.sin(degInRad) * _radius);
+		}
+		gl.glEnd();
 	}
-	
+
 	@Override
 	public int getX() {
-		if(_linkedEntity != null){
-			return _linkedEntity.getX() + _linkedEntity.getWidth()/2;
-		}else{
+		if (_linkedEntity != null) {
+			return _linkedEntity.getX() + _linkedEntity.getWidth() / 2;
+		} else {
 			return super.getX();
 		}
 	}
-	
+
 	@Override
 	public int getY() {
-		if(_linkedEntity != null){
-			return _linkedEntity.getY() + _linkedEntity.getHeight()/2;
-		}else{
+		if (_linkedEntity != null) {
+			return _linkedEntity.getY() + _linkedEntity.getHeight() / 2;
+		} else {
 			return super.getY();
 		}
 	}
-	
-	
-	public void linkToEntity(AbstractEntity pEntity){
+
+	public void linkToEntity(AbstractEntity pEntity) {
 		_linkedEntity = pEntity;
 	}
-	
-	public void setRadius(int pRadius){
+
+	public void setRadius(int pRadius) {
 		_radius = pRadius;
 	}
-	
-	public int getRadius(){
+
+	public int getRadius() {
 		return _radius;
 	}
 }
